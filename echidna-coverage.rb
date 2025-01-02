de653d54dc3bee60681d067f72290a8a016d8bf6
Simon Busch <Simon.busch@live.fr>
2025-01-02 09:00:14 +0100

    update sha

--- echidna-coverage.rb
+++ echidna-coverage.rb
@@ -2,7 +2,7 @@ class EchidnaCoverage < Formula
   desc "Coverage report generator for Echidna"
   homepage "https://github.com/Simon-Busch/echidna-coverage"
   url "https://github.com/Simon-Busch/echidna-coverage/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "316cbb5dc8e1342e601cc7aa27bef53abcb966e455e75354a14ba2eb6eb4a68c"
+  sha256 "e9540c9fdd42b08ac8cf94e4102a60025442d63d1f5183e18a9953c13143b15b"
   license "MIT"
 
   depends_on "node@18"
